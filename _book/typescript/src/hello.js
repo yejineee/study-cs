503d1ddffb0581ef71fc41abecc4907c6213a8b4
yejineee <yygeniee@gmail.com>
2021-08-02 11:53:44 +0900

    Delete: _book 삭제

--- _book/typescript/src/hello.js
+++
@@ -1,5 +0,0 @@
-function greet(person, date) {
-    var a;
-    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
-}
-greet("Maddison", new Date());
--- _book/typescript/src/hello.ts
+++
@@ -1,6 +0,0 @@
-function greet(person: string, date: Date) {
-  let a ;
-  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
-
-greet("Maddison", new Date());
